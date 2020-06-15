45e2b932d6673c246bb9b7c2ad9f0dd5b7e85348
Shaunna Beck <ShaunnaBeck@Shaunnas-MacBook-Pro.local>
2020-06-15 15:38:58 -0400

    setvalue and setUseEffectValue.

--- src/hooks/hooks_container1.js
+++ src/hooks/hooks_container1.js
@@ -1,10 +1,17 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 const HooksContainer1 = () => {
 
     //const stateValue = useState(0)[0]
     //const setValue = useState(0)[1]
     const [stateValue, setValue] = useState(0)
+    const [useEffectValue, setUseEffectValue] = useState(null)
+
+    useEffect(() => {
+        setTimeout(() => 
+            setUseEffectValue("useEffect Worked")
+        , 2000);
+    },[stateValue])
 
 
     const incrementValue = () => {
@@ -15,14 +22,24 @@ const HooksContainer1 = () => {
         setValue(stateValue - 1)
     }
 
+    const changeuseEffectValue = () => {
+        setUseEffectValue('some string')
+    }
+
     return(
         <div>
             React hooks
             <br />
             <button onClick={() => incrementValue()}>increment local state</button>
             <button onClick={() => decrementValue()}>decrement local state</button>
+            <button onClick={() => changeuseEffectValue()}>change useEffect value</button>
             <br />
             <div>
+               {useEffectValue
+                    ? <p>{useEffectValue}</p>
+                    : <p>no value</p>
+                   }
+                <br />
                 <p>local state: {stateValue}</p>
             </div>
         </div>
